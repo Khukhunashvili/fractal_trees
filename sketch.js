4000331ae814234f80b6d231a281181d976e57ab
FiNkacho <sabaxuxunashvili@gmail.com>
2018-12-27 21:58:10 +0400

    fractal tree

--- sketch.js
+++ sketch.js
@@ -5,9 +5,30 @@ function setup() {
 function draw() {
   background(20);
   stroke(255);
-  text("Initial commit!", windowWidth/2, windowHeight/2, windowWidth, windowHeight);
-  fill(255);
 
+  translate(windowWidth/2, windowHeight);
+  splitBranch(250);
+  frame(30);
+}
+
+function splitBranch(length){
+  if(length<5) {
+    console.log("Branch is so small now!");
+    return;
+  };
+
+  line(0, 0, 0, -length);
+  translate(0, -length);
+
+  push();
+  rotate(PI/4);
+  splitBranch(length*0.7);
+  pop();
+
+  push();
+  rotate(-PI/4);
+  splitBranch(length*0.7);
+  pop();
 }
 
 window.onresize = function() {
